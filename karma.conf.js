0272d45da808b82c738e7506fd76f0f22d421ccf
jerme404 <clifton.jeremy@gmail.com>
2018-04-04 11:36:39 -0400

    Travis karma launcher

--- karma.conf.js
+++ karma.conf.js
@@ -2,79 +2,85 @@
 // Generated on Thu Nov 30 2017 10:22:56 GMT-0500 (EST)
 
 module.exports = function(config) {
-  config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    let configuration = {
 
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'jasmine-matchers'],
+        // custom launcher for travis.
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
 
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine', 'jasmine-matchers'],
 
-    // list of files / patterns to load in the browser
-    files: [
-        /* Dependencies */
-        './node_modules/angular/angular.js',
-        './node_modules/angular-mocks/angular-mocks.js',
+        // list of files / patterns to load in the browser
+        files: [
+            /* Dependencies */
+            './node_modules/angular/angular.js',
+            './node_modules/angular-mocks/angular-mocks.js',
 
-        /* App files */
-        './src/app/trtl.i18n.module.js',
-        './src/app/trtl.app.module.js',
-        './src/app/trtl.i18n.strings.mock.js',
-        './src/app/**/*.controller.js',
+            /* App files */
+            './src/app/trtl.i18n.module.js',
+            './src/app/trtl.app.module.js',
+            './src/app/trtl.i18n.strings.mock.js',
+            './src/app/**/*.controller.js',
 
-        /* Test files */
-        './src/app/**/*.test.js'
-    ],
+            /* Test files */
+            './src/app/**/*.test.js'
+        ],
 
+        // list of files to exclude
+        exclude: [
+        ],
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+        },
 
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['spec'],
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // web server port
+        port: 9876,
 
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-    // web server port
-    port: 9876,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
 
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+    };
 
+    // Set custom configuration for travis.
+    if (process.env.TRAVIS) {
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
 
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+    config.set(configuration);
+};
