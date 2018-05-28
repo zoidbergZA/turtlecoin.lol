564abf4e89a561b147f186bd197ef2c45ffb6082
ar-x <37969038+ar-x@users.noreply.github.com>
2018-05-27 21:37:14 -0700

    Do not copy paper-wallet to build output via build script, as its already copied automatically by Jekyll

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,15 @@ set -euox pipefail
 
 echo "Build starting"
 
+# Get the latest turtlecoin/paper-turtle
+git submodule update --recursive --remote --merge
+
+# Generate the static site
 rm -rf ./_site
 bundle exec jekyll build
-git submodule update --recursive --remote --merge
-cp -r paper-turtle _site
+
+# Setup paper-turtle (auto copied by Jekyll) in the build folder
+rm -rf _site/paper-turtle/.git
 mv _site/paper-turtle _site/wallet
 
 #bundle exec rake tests
