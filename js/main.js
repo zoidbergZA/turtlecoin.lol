675b57e51d3b2a9f78781eb4e2b23b05a3aa68f6
ar-x <37969038+ar-x@users.noreply.github.com>
2018-05-31 22:27:28 -0700

    Use CSS (instead of attempted/un-used JS) to set div children to the same height

--- js/main.js
+++ js/main.js
@@ -1,46 +1,7 @@
-/* ---- set an even height for a set of elements ---- */
-equalHeight = function (container) {
-
-    var currentTallest = 0,
-        currentRowStart = 0,
-        rowDivs = [],
-        $el,
-        topPosition = 0;
-    $(container).each(function () {
-
-        $el = $(this);
-        $($el).height('auto');
-        topPostion = $el.position().top;
-
-        if (currentRowStart != topPostion) {
-            for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
-                rowDivs[currentDiv].height(currentTallest);
-            }
-            rowDivs.length = 0; // empty the array
-            currentRowStart = topPostion;
-            currentTallest = $el.height();
-            rowDivs.push($el);
-        } else {
-            rowDivs.push($el);
-            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-        }
-        for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
-            rowDivs[currentDiv].height(currentTallest);
-        }
-    });
-};
-
-/* ---- fix alignment bug with service boxes ---- */
 $(document).ready(function () {
-    equalHeight($('#why .why-item .services-box'));
-
     if ($('body').data('page') === 'index') {
       initMap();
     }
-});
-
-$(window).resize(function () {
-    equalHeight($('#why .why-item .services-box'));
 });
 
 /* ---- mixitup.js our work sorting ---- */
